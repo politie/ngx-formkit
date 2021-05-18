f6337ad5d9b07199e9eb903138d93ecd137b28ec
Xi Cephei <79214049+xi-cephei@users.noreply.github.com>
2021-05-18 17:14:18 +0200

    Fix demos

--- projects/showcase/src/app/app.component.ts
+++ projects/showcase/src/app/app.component.ts
@@ -49,6 +49,8 @@ export class AppComponent {
       message: 'Basic implementation',
       formConfig: simpleFormConfig
     },
+    {
+      title: 'Disabled',
       message: 'Demo to show the option to make a field disabled based on a condition.',
       formConfig: disabledFormConfig
     },
