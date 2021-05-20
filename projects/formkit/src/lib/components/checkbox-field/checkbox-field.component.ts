56125ce2a4424492d8dc857d49129e0823da8b12
Xi Cephei <79214049+xi-cephei@users.noreply.github.com>
2021-05-20 12:46:51 +0200

    Update checkbox field to handle new setup and handle multiple checkboxes with field value only with checked checkboxes

--- projects/formkit/src/lib/components/checkbox-field/checkbox-field.component.ts
+++ projects/formkit/src/lib/components/checkbox-field/checkbox-field.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component, HostBinding, Input } from '@angular
 import { FormArray, FormControl } from '@angular/forms';
 import { ICheckboxesField, ICheckboxField } from '../../models/field.model';
 import { FieldBaseDirective } from '../../directives/field-base/field-base.directive';
+import { Options } from '../../models/form.model';
 
 @Component({
   selector: 'formkit-checkbox-field',
@@ -15,5 +16,15 @@ export class CheckboxFieldComponent extends FieldBaseDirective {
 
   @HostBinding('class') get fieldClasses(): string {
     return this.field?.hasOwnProperty('options') ? 'checkbox-field-multiple' : 'checkbox-field-single';
+  }
+
+  onCheckboxChange(event: Event & { target: HTMLInputElement }, option: Options) {
+    if (event.target.checked && this.control.value.indexOf(option.id) < 0) {
+      this.control.setValue([...this.control.value, ...[option.id]], { emitEvent: true, onlySelf: false });
+    } else {
+      const currentValue = this.control.value.slice();
+      currentValue.splice(this.control.value.indexOf(option.id), 1);
+      this.control.setValue(currentValue, { emitEvent: true, onlySelf: false });
+    }
   }
 }
